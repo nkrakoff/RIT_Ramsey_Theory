180ba705a51f3087fca7df43257630b162a82c81
Noah Krakoff <noahkrakoff@gmail.com>
2019-06-17 05:37:06 +0100

    added compliment filtering

--- utils.c
+++ utils.c
@@ -150,6 +150,16 @@ uint8_t*  Induced_subgraph(int n, int* verts, uint8_t* M, int num_v) {
 		return M_sub;
 }
 
+void compliment(uint8_t* M, int num_v) {
+	for (int i = 0; i< num_v; i++) {
+		for (int j = 0; j<num_v; j++) {
+			if (i!=j) {
+				set_M(i,j,num_v,M, 1-get_M(i,j,num_v, M));
+			}
+		}
+	}
+}
+
 
 int find_shared(int i, int j, uint8_t* M, int num_v, int* verts) {
 	//finds the shared neighbors of i and j and puts them into verts and returns the size of verts
@@ -199,35 +209,51 @@ int jkFree(int num_v, uint8_t* M, int k) {
 }
 
 
-int g6_check_jk_free(char* g6, int k) {
-	//checks to see if the graph represented in g6 format is J_k free
+int g6_check_jk_free(char* g6, int k, int k_0) {
+	//checks to see if the graph represented in g6 format is J_k free and its compliment is J_k_0 free
 	int num_v = get_num_vertices(g6);
 	uint8_t* M = convert_to_Matrix(g6);
-	int result = jkFree(num_v, M, k);
+	int result = 1;
+	int count = 0;
+	if (k!=-1) {
+		result = jkFree(num_v, M, k);
+
+	}
+	if (k_0!=-1 && result == 1) {
+		compliment(M, num_v);
+		result = jkFree(num_v, M, k_0);
+	}
 	free(M);
 	return result;
 }
 
 
-void filter(int k, const char* in_file, const char* out_file) {
-	//filters through in_file and writes to out_file(or prints) all graphs who are jk_free
+
+
+
+void filter(int k, int k_0, const char* in_file, const char* out_file) {
+	//filters through in_file and writes to out_file(or prints) all graphs who are jk_free and J_k_0 free in the compliment
 	FILE* ifile = open_file_r(in_file);
 	char* g6 = malloc(sizeof(char)*255);
+	int total = 0;
 	if (out_file!=NULL) {
 		FILE* ofile = open_file_w(out_file);
 		while (get_next_line(ifile, g6)==1) {
-			if (g6_check_jk_free(g6, k)==1) {
+			if (g6_check_jk_free(g6, k,k_0)==1) {
 				write_next_line(ofile,g6);
+				total++;
 			}
 		}
 		close_file(ofile);
 	} else {
 		while (get_next_line(ifile, g6)==1) {
-			if (g6_check_jk_free(g6, k)==1) {
+			if (g6_check_jk_free(g6, k,k_0)==1) {
 				printf("%s",g6);
+				total++;
 			}
 		}
 	}
+	printf("%d\n",total);
 	close_file(ifile);
 	free(g6);
 	
@@ -236,16 +262,33 @@ void filter(int k, const char* in_file, const char* out_file) {
 
 
 int main( int argc, const char* argv[] )
-{
+{	
+	
 	if (argc<3) {
 		return 0;
 	}
 	const char* k_char = argv[1];
-	int k = atoi(k_char);
-	if (argc==3) {
-		filter(k,argv[2], NULL);
+	int count = 0;
+	while (*(k_char + count) != 'C' && *(k_char + count) != '\0') {
+		count++;
+	}
+	
+	int k_0=0;
+	if (*(k_char + count)!='\0') {
+		k_0 = atoi(k_char + count+1);
 	} else {
-		filter(k, argv[2], argv[3]);
+		k_0 = -1;
+	}
+	int k = atoi(k_char);
+	if (k==0) {
+		k=-1;
+	}
+	printf("%d\n%d\n", k, k_0);
+	
+	if (argc==3) {
+		filter(k,k_0,argv[2], NULL);
+	} else {
+		filter(k,k_0, argv[2], argv[3]);
 	}
 	
 	
